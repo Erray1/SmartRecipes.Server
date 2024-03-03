e77044e43405fe6c27bfd1ea53ce220dc8cfe092
AnnSolovey <anny.solovey2.0@yandex.ru>
2024-03-04 00:25:36 +0300

    Добавила компоненты форм входа и регистрации

--- HTTPModels/Accounts/RegisterModel.cs
+++ HTTPModels/Accounts/RegisterModel.cs
@@ -4,6 +4,5 @@ public sealed class RegisterModel
     public string Email { get; set; }
     public string Password { get; set; }
     public string ConfirmPassword { get; set; }
-    public string Address { get; set; }
 }
 
